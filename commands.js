a0f8995a0ba697e8fcbcb74047e0a5e24d8a0598
336699c <63760951+336699c@users.noreply.github.com>
2022-06-26 19:30:07 +0800

    Update commands.js

--- commands.js
+++ commands.js
@@ -4,6 +4,14 @@ _144(e.player.name,e.message,e.player.uuid().substring(0,3),e.player)
 }
 });
 
+function _144_msg(e,m){
+if(m){
+Call.sendMessage("[[[pink]BOT[white] 144_]: "+e);
+}else{
+Chat.sout.writeUTF("%[[[pink]BOT[white] 144_]: "+e);
+}
+}
+
 function _144(pname,ptext,uuid,player){
   switch(ptext){
   case "_query":
@@ -11,9 +19,9 @@ function _144(pname,ptext,uuid,player){
 
 	_gamedata = JSON.parse(Vars.dataDirectory.child("144/gamedata").readString());
 	if(_gamedata[uuid]){
-		Call.sendMessage("[[[pink]BOT[white] 144_]: 玩家 "+uuid+" 账户结余: "+Math.round(_gamedata[uuid]["money"])+"  | 玩家等级: "+_gamedata[uuid]["core"]+" ");
+		_144_msg("玩家 "+uuid+" 账户结余: "+Math.round(_gamedata[uuid]["money"])+"  | 玩家等级: "+_gamedata[uuid]["core"]+" ",player);
 	}else{
-	Call.sendMessage("[[[pink]BOT[white] 144_]: 无此玩家 "+uuid+" 请输入_reg 注册");
+	_144_msg("无此玩家 "+uuid+" 请输入_reg 注册",player);
 	}
 	}catch(error){Vars.dataDirectory.child("144/logs").writeString(error+"\n",true)}
 break;
@@ -21,9 +29,9 @@ break;
 			try{
 			_gamedata = JSON.parse(Vars.dataDirectory.child("144/gamedata").readString());
 			if(_gamedata[uuid]){
-				Call.sendMessage("[[[pink]BOT[white] 144_]: 玩家 "+uuid+" 账户结余: "+_gamedata[uuid]["money"]+" ");
+				_144_msg("玩家 "+uuid+" 账户结余: "+_gamedata[uuid]["money"]+" ",player);
 			}else{
-				Call.sendMessage("[[[pink]BOT[white] 144_]: 玩家 "+uuid+" 开启了新账户！");
+				_144_msg("玩家 "+uuid+" 开启了新账户！",player);
 				_gamedata[uuid]={"name":decodeColor(pname),"money":50,"copper":0,"bp":[],"core":1,"unit":[],"mine":0,"sign":0,"reg":new Date().toDateString()};
 				Vars.dataDirectory.child("144/gamedata").writeString(JSON.stringify(_gamedata));
 			}
